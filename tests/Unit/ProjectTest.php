21d46381798fd658070c0fedfb22d6c26b2ea0c3
fuminopen <fuminopen@gmail.com>
2022-06-14 14:33:42 +0900

    [E12]test for project can add task

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -37,4 +37,16 @@ class ProjectTest extends TestCase
 
         $this->assertInstanceOf(User::class, $project->owner);
     }
+
+    /**
+     * @test
+     */
+    public function it_can_add_a_task()
+    {
+        $project = $this->project->factory()->create();
+
+        $project->addTask('Test task');
+
+        $this->assertCount(1, $project->tasks);
+    }
 }
